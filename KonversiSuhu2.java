19683e702a13cb2f7d02c73a057b1a9fd5a6da78
jansv7 <agustinusjanssen@gmail.com>
2025-09-16 18:42:00 +0700

    Add files via upload

---
+++ KonversiSuhu2.java
@@ -0,0 +1,6 @@
+
+public class KonversiSuhu2 extends KonversiSuhu{
+	public double fahrenheitToReamur(double f) {
+        return (f - 32) * 4/9.0;
+    }
+}
---
+++ Matematika.java
@@ -0,0 +1,14 @@
+public class Matematika {
+	public int pertambahan (int a, int b) {
+		return a+b;
+	}
+	public int pengurangan (int a, int b) {
+		return a-b;
+	}
+	public int perkalian (int a, int b) {
+		return a*b;
+	}
+	public int pembagian (int a, int b) {
+		return a/b;
+	}
+}
