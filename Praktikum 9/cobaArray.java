aa3027c92359a29e599046bb36b6e8c41ff7c66e
jansv7 <agustinusjanssen@gmail.com>
2025-10-25 12:05:59 +0700

    Add files via upload

---
+++ Praktikum 9/cobaArray.java
@@ -0,0 +1,75 @@
+package mencoba;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class cobaArray {
+	public static void main (String [] args) {
+		ArrayList arr = new ArrayList();
+		arr.add("Emily");
+		arr.add("Bob");
+		arr.add(20);
+		arr.add(null);
+		arr.add("Jans");
+//		System.out.println(arr);
+		
+		ArrayList arr1 = new ArrayList();
+		arr1.add("Jesika");
+		arr1.add("Rey");
+		arr1.add(10);
+		arr1.add(null);
+		arr1.add("Agus");
+//		System.out.println(arr1);
+		
+//		arr.addAll(arr1);
+//		System.out.println(arr);
+//		
+//		arr1.addAll(2, arr);
+//		System.out.println(arr1);
+		
+//		arr.set(0, "Deisy");
+//		System.out.println(arr);
+//		
+//		arr1.set(3, 30);
+//		System.out.println(arr1);
+		
+//		arr.remove("Jans");
+//		System.out.println(arr);
+//		
+//		Object testing = arr.remove(0);
+//		System.out.println(testing);
+//		System.out.println(arr);
+		
+//		Object J = arr.get(1);
+//		System.out.println("at index 2 in Array1 number is: " + J);
+		
+//		for (int i=0; i<arr.size(); i++) {
+//			System.out.print(arr.get(i) + " ");
+//		}
+//		
+//		System.out.println();
+//		
+//		for(Object o : arr1) {
+//			System.out.print(o + " ");
+//		}
+		
+//		arr.add(2, "Alpin");
+//		System.out.println(arr);
+		
+		ArrayList<Integer> arr2 = new ArrayList<Integer>();
+		arr2.add(50);
+		arr2.add(40);
+		arr2.add(30);
+		arr2.add(20);
+		
+//		System.out.println("Before sorting list: ");
+//		System.out.println(arr2);
+//		Collections.sort(arr2);
+//		System.out.println("After sorting list: ");
+//		System.out.println(arr2);
+		
+//		System.out.println(arr2);
+//		int b = arr2.size();
+//		System.out.println("The size is: " + b);
+		
+	}
+}
